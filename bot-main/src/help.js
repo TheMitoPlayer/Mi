af6f7b6655b7e97737d1c1a37889520b35553838
TheMitoPlayer <82124079+TheMitoPlayer@users.noreply.github.com>
2021-10-30 23:21:34 -0300

    Add files via upload

---
+++ bot-main/src/help.js
@@ -0,0 +1,25 @@
+﻿const help = (prefix) => {
+	return `
+
+Bem vindo ao meu bot
+Criado por Isaac(Sim, com 500 lições atrasadas eu decidi programar)
+
+*COMANDOS PRA FIGURINHAS:*
+Comandos: *${prefix}sticker* OU *${prefix}stiker
+Resumo: Converte imagem/gif/video em figurinha, para usar envie uma imagem com a legenda *${prefix}sticker* ou responda a image/gif/video com o mesmo comando.
+
+Comando: *${prefix}toimg*
+Resumo: Transforma uma figurinha em imagem, para utilizar responda a figurinha com o comando *${prefix}toimg*
+
+*COMANDOS PARA USAR EM GRUPOS:*
+Comando: *${prefix}add*
+Resumo: adicione uma pessoa ao grupo pelo bot, utilize o comando *${prefix}add 1196xxxxxxx*
+
+
+Mais comandos em breve
+COPYRIGHT TMP 2021
+Todos os direitos reservados
+`
+}
+
+exports.help = help
